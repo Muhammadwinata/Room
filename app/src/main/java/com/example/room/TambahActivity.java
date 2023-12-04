5544929da6dbd121a15a61b2c4037f27f589f2d2
Muhammadwinata <ilhambobi96@gmail.com>
2023-12-04 17:04:54 +0700

    prakterakhir

--- app/src/main/java/com/example/room/TambahActivity.java
+++ app/src/main/java/com/example/room/TambahActivity.java
@@ -2,6 +2,7 @@ package com.example.room;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,8 @@ public class TambahActivity extends AppCompatActivity {
     EditText etNama, etNPM;
     Button btnSimpan;
     AppDatabase database;
+    int id = 0;
+    boolean isEdit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +29,30 @@ public class TambahActivity extends AppCompatActivity {
         btnSimpan = findViewById(R.id.btnSimpan);
         database = AppDatabase.getInstance(getApplicationContext());
 
+        Intent intent = getIntent();
+        id = intent.getIntExtra("id", 0);
+        if (id > 0) {
+            isEdit = true;
+            Mahasiswa mahasiswa = database.mahasiswaDao().get(id);
+            etNama.setText(mahasiswa.fullName);
+            etNPM.setText(mahasiswa.npm);
+        } else {
+            isEdit = false;
+        }
+
         btnSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Mahasiswa mahasiswa = new Mahasiswa();
                 mahasiswa.fullName = etNama.getText().toString();
                 mahasiswa.npm = etNPM.getText().toString();
-                database.mahasiswaDao().insertAll(mahasiswa);
+
+                if (isEdit) {
+                    database.mahasiswaDao().update(id, mahasiswa.fullName, mahasiswa.npm);
+                } else {
+                    database.mahasiswaDao().insertAll(mahasiswa);
+                }
+
                 finish();
             }
         });
